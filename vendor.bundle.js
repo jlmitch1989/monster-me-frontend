217bc253fd895cb3f8acd1432299d10a9345120b
jlmitch1989 <joshuamitchell@Joshuas-MacBook-Pro.local>
2016-09-16 18:25:18 -0400

    Merge new branch for restructuring handlebars
    fixed my handlebars for the user creations, all creation, and artist pages.

--- vendor.bundle.js
+++ vendor.bundle.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(51);
+	module.exports = __webpack_require__(48);
 
 
 /***/ },
@@ -9926,7 +9926,7 @@
 
 /***/ },
 
-/***/ 51:
+/***/ 48:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
